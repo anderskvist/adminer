8e2ac0d1e7bd248c33ab6d6c27ce45300b5d172a
Anders Kvist <ak@cego.dk>
2017-02-24 14:43:06 +0100

    Simple add of forced SSL when connecting via mysqli

--- adminer/drivers/mysql.inc.php
+++ adminer/drivers/mysql.inc.php
@@ -16,6 +16,7 @@ if (!defined("DRIVER")) {
 			function connect($server = "", $username = "", $password = "", $database = null, $port = null, $socket = null) {
 				mysqli_report(MYSQLI_REPORT_OFF); // stays between requests, not required since PHP 5.3.4
 				list($host, $port) = explode(":", $server, 2); // part after : is used for port or socket
+				$this->ssl_set(null, null, '/etc/mysql/ssl/ca.pem', null, null);
 				$return = @$this->real_connect(
 					($server != "" ? $host : ini_get("mysqli.default_host")),
 					($server . $username != "" ? $username : ini_get("mysqli.default_user")),
